9d0f773424e1dfcc2e49025d5562c64414655f81
camila-falaschi <falaschi.camila@yahoo.com.br>
2023-03-14 22:08:57 -0300

    :white_check_mark: Chore: Implement a function to scrape a news page and return a dictionary with the fetched data.

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -22,8 +22,8 @@ def scrape_updates(html_content):
     selector = Selector(html_content)
     urls = []
 
-    for url in selector.css('h2.entry-title'):
-        href = url.css('a::attr(href)').get()
+    for url in selector.css("h2.entry-title"):
+        href = url.css("a::attr(href)").get()
         urls.append(href)
 
     return urls
@@ -32,14 +32,37 @@ def scrape_updates(html_content):
 # Requisito 3
 def scrape_next_page_link(html_content):
     selector = Selector(html_content)
-    next_page_url = selector.css('div.nav-links > a.next::attr(href)').get()
+    next_page_url = selector.css("div.nav-links > a.next::attr(href)").get()
 
     return next_page_url
 
 
 # Requisito 4
 def scrape_news(html_content):
-    """Seu código deve vir aqui"""
+    selector = Selector(html_content)
+    page_url = selector.css("head > link[rel*=canonical]::attr(href)").get()
+    title = selector.css("h1.entry-title::text").get()
+    timestamp = selector.css("ul.post-meta > li:nth-child(2)::text").get()
+    writer = selector.css("span.author > a::text").get()
+    reading_time = selector.css("li.meta-reading-time::text").get()
+    summary_list = selector.css(
+        "div.entry-content > p:first-of-type *::text"
+    ).getall()
+    summary = ""
+    for phrase in summary_list:
+        summary += phrase
+
+    category = selector.css("a.category-style > span.label::text").get()
+
+    return {
+        "url": page_url,
+        "title": title.strip(),
+        "timestamp": timestamp,
+        "writer": writer,
+        "reading_time": int(reading_time.split()[0]),
+        "summary": summary.strip(),
+        "category": category,
+    }
 
 
 # Requisito 5
