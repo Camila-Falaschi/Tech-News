8a4e63cdd0251380f0e869705d0594c74f43094c
camila-falaschi <falaschi.camila@yahoo.com.br>
2023-03-21 09:36:14 -0300

    :white_check_mark: Chore: Implementing 'search_by_title' function to find all news in database according their title

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -1,6 +1,13 @@
+from tech_news.database import search_news
+
+
 # Requisito 7
 def search_by_title(title):
-    """Seu código deve vir aqui"""
+    response = search_news(
+        {"title": {"$regex": f".*{title}.*", "$options": "i"}}
+    )
+    news_list = [(news["title"], news["url"]) for news in response]
+    return news_list
 
 
 # Requisito 8
